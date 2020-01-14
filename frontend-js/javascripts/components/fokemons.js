9031bef3eb48b3e3b92f0e1a685a1238bb023dac
Doug Kintop <dkintop@hotmail.com>
2020-01-14 13:22:22 -0500

    began process of rendering each fokemon to the page in fokemons.js

--- frontend-js/javascripts/components/fokemons.js
+++ frontend-js/javascripts/components/fokemons.js
@@ -7,9 +7,19 @@ class Fokemons {
   }
 
   fetchAndLoadFokemons() {
-    this.adapter.getFokemon().then(fokemons => {
-      console.log(fokemons);
-      return fokemons;
-    });
+    this.adapter
+      .getFokemon()
+      .then(fokemons => {
+        fokemons.forEach(fokemon => this.fokemons.push(fokemon));
+      })
+      .then(() => {
+        this.renderFokemons();
+      });
+  }
+
+  renderFokemons() {
+    const indexContainer = document.getElementById("index-container");
+    indexContainer.innerHTML = "my fokemons here";
+    console.log(`my fokemon are`, this.fokemons);
   }
 }
