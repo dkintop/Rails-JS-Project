33f32744bf68fa46a13b38f13d97b4b6785e04fe
Doug Kintop <dkintop@hotmail.com>
2020-01-13 19:03:40 -0500

    starting over after realizing user authentication with naked javascript was going to be very difficult

--- backend-api/db/migrate/20200113231401_change_default_value_of_wins_and_losses.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDefaultValueOfWinsAndLosses < ActiveRecord::Migration[6.0]
-  def change
-    change_column :Users, :wins, :integer, default: 0
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
--- backend-api/db/migrate/20200113231808_add_defualt_value_to_losses.rb
+++
@@ -1,5 +0,0 @@
-class AddDefualtValueToLosses < ActiveRecord::Migration[6.0]
-  def change
-    change_column :Users, :losses, :integer, default: 0
-  end
-end
