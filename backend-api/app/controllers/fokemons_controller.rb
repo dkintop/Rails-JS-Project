48ba2ab6ce2316fa5b6eef6c02564823c55c6b5e
Doug Kintop <dkintop@hotmail.com>
2020-01-14 09:31:19 -0500

    added index, create, and delete actions to fokemons controller

--- backend-api/app/controllers/fokemons_controller.rb
+++ backend-api/app/controllers/fokemons_controller.rb
@@ -1,7 +1,33 @@
 class FokemonsController < ApplicationController
 
 
-    
+    def create 
+        @fokemon = Fokemon.create(fokemon_params)
+        render json: @fokemon, status: 200
+    end
+
+    def index 
+        @fokemon = Fokemon.all
+        render json: @fokemon, status: 200
+    end
+
+    def update 
+        @fokemon = Fokemon.find(params[:id])
+        @fokemon.update(fokemon_params)
+        render json: @fokemon, status: 200
+    end
 
 
+    private 
+
+    def fokemon_params 
+        params.require(:fokemon).permit(:name, :element_type, :hit_points, :attack_points, :avatar)
+    end
 end
+#  t.string "name"
+#     t.string "element_type", default: "water"
+#     t.integer "hit_points", default: 0
+#     t.integer "attack_points", default: 0
+#     t.string "avatar", default: "https://i.ytimg.com/vi/OxgKvRvNd5o/maxresdefault.jpg"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
