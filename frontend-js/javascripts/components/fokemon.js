434647dd4350aa69bfce632daca51a8429e90724
Doug Kintop <dkintop@hotmail.com>
2020-01-14 15:42:49 -0500

    finished rendering cards to page for each fokemon.

--- frontend-js/javascripts/components/fokemon.js
+++ frontend-js/javascripts/components/fokemon.js
@@ -1,1 +1,10 @@
-
+class Fokemon {
+  constructor(fokemonJson) {
+    this.id = fokemonJson.id;
+    this.name = fokemonJson.name;
+    this.element_type = fokemonJson.element_type;
+    this.hit_points = fokemonJson.hit_points;
+    this.attack_points = fokemonJson.attack_points;
+    this.avatar = fokemonJson.avatar;
+  }
+}
--- frontend-js/javascripts/components/fokemons.js
+++ frontend-js/javascripts/components/fokemons.js
@@ -10,7 +10,7 @@ class Fokemons {
     this.adapter
       .getFokemon()
       .then(fokemons => {
-        fokemons.forEach(fokemon => this.fokemons.push(fokemon));
+        fokemons.forEach(fokemon => this.fokemons.push(new Fokemon(fokemon)));
       })
       .then(() => {
         this.renderFokemons();
@@ -19,7 +19,36 @@ class Fokemons {
 
   renderFokemons() {
     const indexContainer = document.getElementById("index-container");
-    indexContainer.innerHTML = "my fokemons here";
-    console.log(`my fokemon are`, this.fokemons);
+    this.fokemons.forEach(fokemon => this.createCard(fokemon));
+  }
+
+  createCard(fokemon) {
+    let parent = document.createElement("DIV");
+    parent.setAttribute("class", "card");
+
+    let name = document.createElement("DIV");
+    name.setAttribute("id", "fokemon_name");
+    name.innerHTML = `${fokemon.name}`;
+    let element_type = document.createElement("DIV");
+    element_type.setAttribute("id", "element_type");
+    element_type.innerHTML = `${fokemon.element_type}`;
+    let hit_points = document.createElement("DIV");
+    hit_points.setAttribute("id", "hit_points");
+    hit_points.innerHTML = `${fokemon.hit_points}`;
+    let attack_points = document.createElement("DIV");
+    attack_points.setAttribute("id", "attack_points");
+    attack_points.innerHTML = `${fokemon.attack_points}`;
+
+    let avatar = document.createElement("IMG");
+    avatar.setAttribute("id", "avatar");
+    avatar.setAttribute("src", fokemon.avatar);
+
+    const container = document.getElementById("index-container");
+
+    let card_elements = [name, element_type, hit_points, attack_points, avatar];
+
+    card_elements.forEach(element => parent.appendChild(element));
+
+    document.querySelector("#index-container").appendChild(parent);
   }
 }
