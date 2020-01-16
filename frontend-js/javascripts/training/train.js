b4696cfa44d5bac63b5e62e53c246c7aa1fb15fa
Doug Kintop <dkintop@hotmail.com>
2020-01-16 13:10:39 -0500

    added getAvatarSrc() method to Train class in train.js to return avatar src

--- frontend-js/javascripts/training/train.js
+++ frontend-js/javascripts/training/train.js
@@ -2,9 +2,11 @@ class Train {
   constructor(fokemon_id) {
     this.fokemon_id = fokemon_id;
     this.fokemon_adapter = new FokemonAdapter();
-    this.fokemon = [];
-
-    // this.fetchFokemon();
+    this.getAvatarSrc();
   }
   //Train class instance created when clicking train button on cardand passes in an argument of the fokemon id associated with that card. eventlistener and DOM element added in fokemons.js createCard()
+  getAvatarSrc() {
+    let avatar = document.getElementById(`avatar${this.fokemon_id}`);
+    return avatar.getAttribute("src");
+  }
 }
