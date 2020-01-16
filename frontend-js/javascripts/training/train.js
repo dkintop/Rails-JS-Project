052008eb2adb5bee4571df53e978bce1274d69f2
Doug Kintop <dkintop@hotmail.com>
2020-01-16 13:46:38 -0500

    completed function to trigger when train button is clicked that will load the avatar image into the training field

--- frontend-js/javascripts/training/train.js
+++ frontend-js/javascripts/training/train.js
@@ -1,12 +1,21 @@
 class Train {
   constructor(fokemon_id) {
     this.fokemon_id = fokemon_id;
-    this.fokemon_adapter = new FokemonAdapter();
-    this.getAvatarSrc();
+    this.displayAvatarOnTrainingField();
   }
   //Train class instance created when clicking train button on cardand passes in an argument of the fokemon id associated with that card. eventlistener and DOM element added in fokemons.js createCard()
   getAvatarSrc() {
     let avatar = document.getElementById(`avatar${this.fokemon_id}`);
     return avatar.getAttribute("src");
+  }
+
+  displayAvatarOnTrainingField() {
+    let src = this.getAvatarSrc();
+    let container = document.getElementById("avatar-container");
+    let trainingImage = document.createElement("img");
+    trainingImage.setAttribute("src", `${src}`);
+    trainingImage.setAttribute("id", "trainingImage");
+    trainingImage.setAttribute("class", "avatar");
+    container.appendChild(trainingImage);
   }
 }
